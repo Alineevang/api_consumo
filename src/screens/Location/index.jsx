a828fdd23a90c8b3942c821f5a9cecb33ecce705
Aline <123502100+Alineevang@users.noreply.github.com>
2024-04-08 11:51:24 -0300

    feat: consumo API clima

--- src/screens/Location/index.jsx
+++ src/screens/Location/index.jsx
@@ -10,17 +10,30 @@ import { AnimatedMapView } from "react-native-maps/lib/MapView";
 import { MarkerAnimated } from "react-native-maps";
 import { watchPositionAsync } from "expo-location";
 import { LocationAccuracy } from "expo-location";
+import { Axios } from "axios";
+import { ActivityIndicator } from "react-native";
+import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 
 export default function Location() {
   const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
+
   const mapRef = useRef(null);
 
   async function requestPermission() {
-    const { granted } = await requestForegroundPermissionsAsync();
+    const { granted, status } = await requestForegroundPermissionsAsync();
+
+    if (status !== "granted") {
+      setErrorMsg("Permissão para acessar localização foi negada!");
+      console.log("Permissão para acessar localização foi negada!");
+      return;
+    }
 
     if (granted) {
       const currentPosition = await getCurrentPositionAsync();
       setLocation(currentPosition);
+      getWeather(location.coords.latitude, location.coords.longitude);
       console.log("Localização atual", currentPosition);
       return;
     }
@@ -48,10 +61,32 @@ export default function Location() {
     );
   }, []);
 
+  const getWeather = async (latitude, longitude) => {
+    try {
+      console.log("Latitude: ", latitude, "Longitude: ", longitude);
+      const apiKey = "d2b17491b2a840713b385d9b2fb02057";
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+      );
+      console.log(response.data);
+      setWeatherData(response.data);
+    } catch (error) {
+      setErrorMsg(error.message);
+    }
+  };
 
   return (
     <View style={styles.container}>
       <Title title="Location" />
+
+      {weatherData ? (
+        <Text>Temperatura:
+          {weatherData.current.temp / 10}°C</Text>
+      ) : (
+        <ActivityIndicator size="large" color="#0000ff" />
+      )}
+      {errorMsg && <Text>{errorMsg}</Text>}
+
       {location && (
         <AnimatedMapView
           style={styles.map}
